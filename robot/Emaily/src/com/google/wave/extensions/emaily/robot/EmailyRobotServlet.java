44009f490ba11cc2b55338fdbda6cc896b9975c9
Balazs Szabo (dLux) <dlux@dlux.hu>
2009-09-21 00:27:55 +0200

    work in progress.

--- robot/Emaily/src/com/google/wave/extensions/emaily/robot/EmailyRobotServlet.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/robot/EmailyRobotServlet.java
@@ -11,9 +11,8 @@ import com.google.wave.api.Wavelet;
 
 @Singleton
 public class EmailyRobotServlet extends AbstractRobotServlet {
-
 	private static final long serialVersionUID = 8878209094937861353L;
-
+	
 	@Override
 	public void processEvents(RobotMessageBundle bundle) {
 		Wavelet wavelet = bundle.getWavelet();
@@ -21,7 +20,7 @@ public class EmailyRobotServlet extends AbstractRobotServlet {
 		if (bundle.wasSelfAdded()) {
 			Blip blip = wavelet.getRootBlip();
 			TextView view = blip.getDocument();
-			view.replace("Testing");
+			// view.replace("Testing");
 			for (int i = 0; i < 3; ++i) {
 				view.append("\nTo: test" + Integer.toString(i) + "@example.com");
 				view.appendElement(new FormElement(ElementType.CHECK, "remove" + Integer.toString(i), "Remove", "CHECKED"));
@@ -32,7 +31,16 @@ public class EmailyRobotServlet extends AbstractRobotServlet {
 			view.appendElement(new FormElement(ElementType.BUTTON, "not_email", "Not email"));
 			view.append("   ");
 			view.appendElement(new FormElement(ElementType.BUTTON, "add_bcc", "Add Bcc"));
+			// logger.info(view.toString());
 		}
 	}
+
+	// Injected dependencies
+	// private Logger logger;
+	
+	// @Inject
+	// public void setLogger(Logger logger) {
+	//	this.logger = logger;
+	// }
 
 }
