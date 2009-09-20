d0ea1b15edc5931f28ff620302751de27b81dc41
Balazs Szabo (dLux) <dlux@dlux.hu>
2009-09-20 23:03:52 +0200

    Fixes reviewer comments (including making EmailyServletModule an anonymous inner class).

--- robot/Emaily/src/com/google/wave/extensions/emaily/web/EmailyServletContextListener.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/web/EmailyServletContextListener.java
@@ -3,12 +3,19 @@ package com.google.wave.extensions.emaily.web;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+import com.google.wave.extensions.emaily.robot.EmailyRobotServlet;
 
 public class EmailyServletContextListener extends GuiceServletContextListener {
 
 	@Override
 	protected Injector getInjector() {
-		return Guice.createInjector(new EmailyServletModule());
+		return Guice.createInjector(new ServletModule() {
+			@Override
+			protected void configureServlets() {
+				serve("/_wave/robot/jsonrpc").with(EmailyRobotServlet.class);
+			}
+		});
 	}
 
 }
