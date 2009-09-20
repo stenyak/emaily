1db01756aa8d430031f3b45a93313f5467cf2ec0
Balazs Szabo (dLux) <dlux@dlux.hu>
2009-09-20 22:30:35 +0200

    Added profile for the robot.

--- robot/Emaily/src/com/google/wave/extensions/emaily/web/EmailyServletModule.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/web/EmailyServletModule.java
@@ -1,6 +1,7 @@
 package com.google.wave.extensions.emaily.web;
 
 import com.google.inject.servlet.ServletModule;
+import com.google.wave.extensions.emaily.robot.EmailyProfileServlet;
 import com.google.wave.extensions.emaily.robot.EmailyRobotServlet;
 
 public class EmailyServletModule extends ServletModule {
@@ -8,5 +9,6 @@ public class EmailyServletModule extends ServletModule {
 	protected void configureServlets() {
 		super.configureServlets();
 		serve("/_wave/robot/jsonrpc").with(EmailyRobotServlet.class);
+		serve("/_wave/robot/profile").with(EmailyProfileServlet.class);
 	}
 }
