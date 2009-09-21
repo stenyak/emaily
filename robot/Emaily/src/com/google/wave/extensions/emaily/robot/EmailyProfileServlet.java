b6a0631fac3e772e37f972629904678702831594
dlux <devnull@localhost>
2009-09-21 14:43:35 +0200

    Fixes reviewer comments on 5192e61e071bf231c67546b13f3b62f97d123d74.

--- robot/Emaily/src/com/google/wave/extensions/emaily/robot/EmailyProfileServlet.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/robot/EmailyProfileServlet.java
@@ -3,6 +3,10 @@ package com.google.wave.extensions.emaily.robot;
 import com.google.inject.Singleton;
 import com.google.wave.api.ProfileServlet;
 
+/**
+ * Servlet to return the profile information for the robot.
+ * @author dlux
+ */
 @Singleton
 public class EmailyProfileServlet extends ProfileServlet {
 	
@@ -11,6 +15,7 @@ public class EmailyProfileServlet extends ProfileServlet {
 	@Override
 	public String getRobotAvatarUrl() {
 		// TODO(dlux): Put this string to some config file.
+		// TODO(dlux): Find someone who can create an avatar for this project.
 		return "http://1.bp.blogspot.com/_yNL0M57xbfg/Skr1tUorzkI/AAAAAAAAAIU/Texw5ki8Qw8/s320/EMAIL_ICON.jpg";
 	}
 	
