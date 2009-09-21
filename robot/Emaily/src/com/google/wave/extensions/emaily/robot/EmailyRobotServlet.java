a9997ae4d892e10646707614278ee18af7ef8255
Balazs Szabo (dLux) <dlux@dlux.hu>
2009-09-21 17:28:27 +0200

    Work in progress: target-selection-gadget is added to the wave. Needs some cleanup.

--- robot/Emaily/src/com/google/wave/extensions/emaily/robot/EmailyRobotServlet.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/robot/EmailyRobotServlet.java
@@ -1,10 +1,14 @@
 package com.google.wave.extensions.emaily.robot;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.wave.api.AbstractRobotServlet;
+import com.google.wave.api.Annotation;
 import com.google.wave.api.Blip;
-import com.google.wave.api.ElementType;
-import com.google.wave.api.FormElement;
+import com.google.wave.api.Gadget;
 import com.google.wave.api.RobotMessageBundle;
 import com.google.wave.api.TextView;
 import com.google.wave.api.Wavelet;
@@ -18,29 +22,39 @@ public class EmailyRobotServlet extends AbstractRobotServlet {
 		Wavelet wavelet = bundle.getWavelet();
 
 		if (bundle.wasSelfAdded()) {
-			Blip blip = wavelet.getRootBlip();
-			TextView view = blip.getDocument();
-			// view.replace("Testing");
-			for (int i = 0; i < 3; ++i) {
-				view.append("\nTo: test" + Integer.toString(i) + "@example.com");
-				view.appendElement(new FormElement(ElementType.CHECK, "remove" + Integer.toString(i), "Remove", "CHECKED"));
-			}
-			view.append("\n\nHello,\n\n\n");
-			view.appendElement(new FormElement(ElementType.BUTTON, "emailsend", "Send Email"));
-			view.append("   ");
-			view.appendElement(new FormElement(ElementType.BUTTON, "not_email", "Not email"));
-			view.append("   ");
-			view.appendElement(new FormElement(ElementType.BUTTON, "add_bcc", "Add Bcc"));
-			// logger.info(view.toString());
+			handleSelfAdded(wavelet);
 		}
 	}
 
+	private void handleSelfAdded(Wavelet wavelet) {
+		Blip blip = wavelet.getRootBlip();
+		TextView view = blip.getDocument();
+		// Find position for the gadget: Right after the conversation title if any
+		int gadget_position = 0;
+		for (Annotation a: view.getAnnotations("conv/title")) {
+			if (a.getRange().getEnd() > gadget_position) {
+				gadget_position = a.getRange().getEnd();
+			}
+		}
+		view.insertElement(gadget_position, new Gadget("http://2.latest.emaily-wave.appspot.com/gadgets/target-selection-gadget.xml"));
+		StringBuilder blipState = new StringBuilder();
+		blipState.append("Blip content:");
+		blipState.append(view.getText());
+		blipState.append("\nAnnotations:\n");
+		for (Annotation a: view.getAnnotations()) {
+			blipState.append("- ");
+			blipState.append(a.toString());
+			blipState.append("\n");
+		}
+		logger.log(Level.WARNING, blipState.toString());
+	}
+
 	// Injected dependencies
-	// private Logger logger;
+	private Logger logger;
 	
-	// @Inject
-	// public void setLogger(Logger logger) {
-	//	this.logger = logger;
-	// }
+	@Inject
+	public void setLogger(Logger logger) {
+		this.logger = logger;
+	}
 
 }
