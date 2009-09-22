0bd0abae688793557c9f80326dafa434df27a1d2
Balazs Szabo (dLux) <dlux@dlux.hu>
2009-09-22 22:34:19 +0200

    Applied new formatting rules.

--- robot/Emaily/src/com/google/wave/extensions/emaily/robot/EmailyRobotServlet.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/robot/EmailyRobotServlet.java
@@ -12,44 +12,50 @@ import com.google.wave.api.Wavelet;
 
 @Singleton
 public class EmailyRobotServlet extends AbstractRobotServlet {
-	private static final long serialVersionUID = 8878209094937861353L;
-	
-	/**
-	 * Handles all robot events.
-	 */
-	@Override
-	public void processEvents(RobotMessageBundle bundle) {
-		if (bundle.wasSelfAdded()) {
-			handleSelfAdded(bundle);
-		}
-	}
+  private static final long serialVersionUID = 8878209094937861353L;
 
-	/**
-	 * Handles case when the robot is added to the wave. 
-	 * @param bundle All information from the robot event.
-	 */
-	private void handleSelfAdded(RobotMessageBundle bundle) {
-		Wavelet wavelet = bundle.getWavelet();
-		Blip blip = wavelet.getRootBlip();
-		TextView view = blip.getDocument();
-		// Find position for the gadget: Right after the conversation title if any
-		// TODO(dlux): If there is no subject, then keep some space for that.
-		int gadget_position = 0;
-		for (Annotation a: view.getAnnotations("conv/title")) {
-			if (a.getRange().getEnd() > gadget_position) {
-				gadget_position = a.getRange().getEnd();
-			}
-		}
-		// TODO(dlux): Use bundle.getRobotAddress when it is working.
-		view.insertElement(gadget_position, new Gadget("http://2.latest.emaily-wave.appspot.com/gadgets/target-selection-gadget.xml"));
-		debugHelper.DumpWaveletState(wavelet);
-	}
+  /**
+   * Handles all robot events.
+   */
+  @Override
+  public void processEvents(RobotMessageBundle bundle) {
+    if (bundle.wasSelfAdded()) {
+      handleSelfAdded(bundle);
+    }
+  }
 
-	// Injected dependencies
-	privte DebugHelper debugHelper;
-	
-	@Inject
-	public void setDebugHelper(DebugHelper debugHelper) {
-		this.debugHelper = debugHelper;
-	}
+  /**
+   * Handles case when the robot is added to the wave.
+   * 
+   * @param bundle
+   *          All information from the robot event.
+   */
+  private void handleSelfAdded(RobotMessageBundle bundle) {
+    Wavelet wavelet = bundle.getWavelet();
+    Blip blip = wavelet.getRootBlip();
+    TextView view = blip.getDocument();
+    // Find position for the gadget: Right after the conversation title if any
+    // TODO(dlux): If there is no subject, then keep some space for that.
+    int gadget_position = 0;
+    for (Annotation a : view.getAnnotations("conv/title")) {
+      if (a.getRange().getEnd() > gadget_position) {
+        gadget_position = a.getRange().getEnd();
+      }
+    }
+    // TODO(dlux): Use bundle.getRobotAddress when it is working.
+    view
+        .insertElement(
+            gadget_position,
+            new Gadget("http://2.latest.emaily-wave.appspot.com" +
+            		"/gadgets/target-selection-gadget.xml"));
+    debugHelper.DumpWaveletState(wavelet);
+  }
+
+  // Injected dependencies
+  private DebugHelper debugHelper;
+
+  @Inject
+  public void setDebugHelper(DebugHelper debugHelper) {
+    this.debugHelper = debugHelper;
+  }
 }
