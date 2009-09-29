4fe98a02bb7a1ac7f1dcfbb48d06a5bd1ceae3db
taton <devnull@localhost>
2009-09-29 21:06:50 +0200

    Import utilities. Various cleanups. Enable app-engine incoming email. Switch logging to INFO log.

--- robot/Emaily/src/com/google/wave/extensions/emaily/robot/DebugHelper.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/robot/DebugHelper.java
@@ -1,15 +1,19 @@
 package com.google.wave.extensions.emaily.robot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.google.wave.api.Annotation;
 import com.google.wave.api.Blip;
-import com.google.wave.api.Gadget;
-import com.google.wave.api.TextView;
+import com.google.wave.api.Event;
+import com.google.wave.api.EventType;
+import com.google.wave.api.RobotMessageBundle;
 import com.google.wave.api.Wavelet;
+import com.google.wave.extensions.emaily.util.StringPrinter;
 
 /**
  * Utility functions for debugging Wave and Robot interactions
@@ -19,29 +23,85 @@ import com.google.wave.api.Wavelet;
 @Singleton
 public class DebugHelper {
 
-  // TODO(dlux): Add more information from the wavelet.
+  /**
+   * Logs details of a wavelet to the INFO log.
+   * 
+   * @param wavelet The wavelet to log.
+   */
   public void DumpWaveletState(Wavelet wavelet) {
-    Blip blip = wavelet.getRootBlip();
-    TextView view = blip.getDocument();
-    StringBuilder waveletState = new StringBuilder();
-    waveletState.append("Blip content:");
-    waveletState.append(view.getText());
-    waveletState.append("\nAnnotations:\n");
-    for (Annotation a : view.getAnnotations()) {
-      waveletState.append("- ");
-      waveletState.append(a.toString());
-      waveletState.append("\n");
-    }
-    waveletState.append("Gadgets:\n");
-    for (Gadget g : view.getGadgetView().getGadgets()) {
-      waveletState.append("- ");
-      waveletState.append(g.toString());
-      waveletState.append("\n");
-    }
+    StringPrinter sp = new StringPrinter();
+    PrintWaveletInfo(sp, wavelet);
+    PrintBlipInfo(sp, wavelet.getRootBlip());
+    logger.log(Level.INFO, sp.toString());
+  }
 
-    // TODO(dlux): level.INFO is not visible in appengine logs, find out why and
-    // use that level.
-    logger.log(Level.WARNING, waveletState.toString());
+  /**
+   * Appends details of a RobotMessageBundle to the given StringPrinter.
+   * 
+   * @param sp
+   *          The StringPrinter to append info to.
+   * @param bundle
+   *          The RobotMessageBundle to dump.
+   */
+  public static void PrintRobotMessageBundleInfo(StringPrinter sp,
+      RobotMessageBundle bundle) {
+    sp.println("RobotMessageBundle info:");
+    sp.catln("RobotAddress: ", bundle.getRobotAddress());
+    sp.catln("Is new wave? ", bundle.isNewWave());
+    sp.catln("Was self added? ", bundle.wasSelfAdded());
+    sp.catln("Was self removed? ", bundle.wasSelfRemoved());
+    List<EventType> types = new ArrayList<EventType>();
+    for (Event event : bundle.getEvents())
+      types.add(event.getType());
+    sp.print("Events: ").joinln(types, ", ");
+  }
+
+  /**
+   * Appends details of a Blip to the given StringPrinter.
+   * 
+   * @param sp
+   *          The StringPrinter to append info to.
+   * @param blip
+   *          The Blip MessageBundle to dump.
+   */
+  public static void PrintBlipInfo(StringPrinter sp, Blip blip) {
+    sp.catln("Blip info for blip ID: ", blip.getBlipId());
+    sp.catln("Parent blip ID: ", blip.getParentBlipId());
+    sp.catln("Creator: ", blip.getCreator());
+    sp.catln("Last modified time: ", blip.getLastModifiedTime());
+    sp.print("Contributors: ").joinln(blip.getContributors(), ", ");
+    sp.catln("Document text:", blip.getDocument().getText());
+    sp.print("Annotations: ").joinln(blip.getDocument().getAnnotations(), ", ");
+    sp.print("Gadgets: ").joinln(
+        blip.getDocument().getGadgetView().getGadgets(), ", ");
+    sp.print("Children blip IDs: ").joinln(blip.getChildBlipIds(), ", ");
+    for (Blip child : blip.getChildren())
+      PrintBlipInfo(sp, child);
+    sp.catln("End of blip info for blip ID: ", blip.getBlipId());
+  }
+
+  /**
+   * Appends details of a Wavelet to the given StringPrinter.
+   * 
+   * @param sp
+   *          The StringPrinter to append info to.
+   * @param wavelet
+   *          The Wavelet to dump.
+   */
+  public static void PrintWaveletInfo(StringPrinter sp, Wavelet wavelet) {
+    sp.println("Wavelet info:");
+    sp.catln("title: ", wavelet.getTitle());
+    sp.catln("ID: ", wavelet.getWaveletId());
+    sp.catln("Wave ID: ", wavelet.getWaveId());
+    sp.catln("Creator: ", wavelet.getCreator());
+    sp.catln("Creation time: ", wavelet.getCreationTime());
+    sp.catln("Last modified time: ", wavelet.getLastModifiedTime());
+    sp.catln("Version: ", wavelet.getVersion());
+    sp.print("Participants: ").joinln(wavelet.getParticipants(), ", ");
+    for (Map.Entry<String, String> entry : wavelet.getDataDocuments()
+        .entrySet())
+      sp.catln("Data document: ", entry.getKey(), " : ", entry.getValue());
+    sp.catln("Root blip ID: ", wavelet.getRootBlipId());
   }
 
   // Injected dependencies
