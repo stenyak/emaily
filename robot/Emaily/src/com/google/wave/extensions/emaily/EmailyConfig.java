ba784d4c478c8bbb7497b8b624b4fc9c30b827b1
dlux <devnull@localhost>
2009-10-08 23:27:13 +0200

    Some tweaks. It is looking a little bit better.

--- robot/Emaily/src/com/google/wave/extensions/emaily/EmailyConfig.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/EmailyConfig.java
@@ -1,19 +1,10 @@
 package com.google.wave.extensions.emaily;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
-import javax.servlet.ServletContext;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
 
 /**
  * Class for configuring Emaily.
@@ -21,62 +12,24 @@ import org.xml.sax.SAXException;
  * @author dlux
  * 
  */
+@Singleton
 public class EmailyConfig {
-  // Constants for properties
+  // Constants for property names
   public static final String APPENGINE_APP_NAME = "appengine.app_name";
   public static final String APPENGINE_APP_VERSION = "appengine.app_version";
 
-  /**
-   * Loads properties from the global and local properties files and returns
-   * them as a properties object.
-   * 
-   * @param servletContext
-   * @return
-   */
-  public static Properties loadProperties(ServletContext servletContext) {
-    // Load the global emaily.properties file
-    Properties props = new Properties();
-    InputStream globalPropInputStream = servletContext
-        .getResourceAsStream("/WEB-INF/emaily.properties");
-    try {
-      props.load(globalPropInputStream);
-    } catch (IOException e) {
-      throw new RuntimeException("Cannot load emaily.properties", e);
-    }
-    // Load the local (optional) properties file (to override global properties)
-    InputStream localPropInputStream = servletContext
-        .getResourceAsStream("/WEB-INF/emaily-local.properties");
-    if (localPropInputStream != null) {
-      try {
-        props.load(localPropInputStream);
-      } catch (IOException e) {
-        throw new RuntimeException("Cannot load emaily-local.properties");
-      }
-    }
-    // Set AppEngine properties from appengine-web.xml
-    InputStream appengineXmlInputStream = servletContext
-        .getResourceAsStream("/WEB-INF/appengine-web.xml");
-    if (appengineXmlInputStream != null) {
-      try {
-        Document appengineXml = DocumentBuilderFactory.newInstance()
-            .newDocumentBuilder().parse(appengineXmlInputStream);
-        XPath xPath = XPathFactory.newInstance().newXPath();
-        Object appName = xPath.compile("//*[local_name()='application']")
-            .evaluate(appengineXml, XPathConstants.STRING);
-        props.setProperty(APPENGINE_APP_NAME, (String) appName);
-        Object appVersion = xPath.compile("//*[local_nam,e()='version']")
-            .evaluate(appengineXml, XPathConstants.STRING);
-        props.setProperty(APPENGINE_APP_VERSION, (String) appVersion);
-      } catch (SAXException e) {
-        throw new RuntimeException("Cannot parse appengine-web.xml", e);
-      } catch (IOException e) {
-        throw new RuntimeException("Error reading appengine-web.xml", e);
-      } catch (ParserConfigurationException e) {
-        throw new RuntimeException("Error during XML parsing", e);
-      } catch (XPathExpressionException e) {
-        throw new RuntimeException("Error during XML parsing", e);
-      }
-    }
-    return props;
+  private Properties properties;
+  
+  @Inject
+  EmailyConfig(@Named("ServletProperties") Properties properties) {
+    this.properties = properties;
+  }
+  
+  public String get(String key) {
+    return properties.getProperty(key);
+  }
+  
+  public String get(String key, String defaultValue) {
+    return properties.getProperty(key, defaultValue);
   }
 }
--- robot/Emaily/src/com/google/wave/extensions/emaily/web/EmailyServletContextListener.java
+++ robot/Emaily/src/com/google/wave/extensions/emaily/web/EmailyServletContextListener.java
@@ -1,12 +1,25 @@
 package com.google.wave.extensions.emaily.web;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.name.Names;
+import com.google.inject.Provides;
+import com.google.inject.name.Named;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
 import com.google.wave.extensions.emaily.EmailyConfig;
@@ -20,9 +33,8 @@ public class EmailyServletContextListener extends GuiceServletContextListener {
 
   @Override
   public void contextInitialized(ServletContextEvent servletContextEvent) {
+    properties = loadProperties(servletContextEvent.getServletContext());
     super.contextInitialized(servletContextEvent);
-    properties = EmailyConfig.loadProperties(servletContextEvent
-        .getServletContext());
   }
 
   @Override
@@ -33,9 +45,68 @@ public class EmailyServletContextListener extends GuiceServletContextListener {
         serve("/_wave/robot/jsonrpc").with(EmailyRobotServlet.class);
         serve("/_wave/robot/profile").with(EmailyProfileServlet.class);
         serve("/_ah/mail/*").with(IncomingEmailServlet.class);
-        Names.bindProperties(binder(), properties);
       }
+      
+      @SuppressWarnings("unused")
+      @Provides @Named("ServletProperties")
+      public Properties getEmailyProperties() {
+        return properties;
+      }
+           
     });
   }
 
+  /**
+   * Loads properties from the global and local properties files and returns
+   * them as a properties object.
+   * 
+   * @param servletContext
+   * @return
+   */
+  public Properties loadProperties(ServletContext servletContext) {
+    // Load the global emaily.properties file
+    Properties props = new Properties();
+    InputStream globalPropInputStream = servletContext
+        .getResourceAsStream("/WEB-INF/emaily.properties");
+    try {
+      props.load(globalPropInputStream);
+    } catch (IOException e) {
+      throw new RuntimeException("Cannot load emaily.properties", e);
+    }
+    // Load the local (optional) properties file (to override global properties)
+    InputStream localPropInputStream = servletContext
+        .getResourceAsStream("/WEB-INF/emaily-local.properties");
+    if (localPropInputStream != null) {
+      try {
+        props.load(localPropInputStream);
+      } catch (IOException e) {
+        throw new RuntimeException("Cannot load emaily-local.properties");
+      }
+    }
+    // Set AppEngine properties from appengine-web.xml
+    InputStream appengineXmlInputStream = servletContext
+        .getResourceAsStream("/WEB-INF/appengine-web.xml");
+    if (appengineXmlInputStream != null) {
+      try {
+        Document appengineXml = DocumentBuilderFactory.newInstance()
+            .newDocumentBuilder().parse(appengineXmlInputStream);
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        Object appName = xPath.compile("//*[local_name()='application']")
+            .evaluate(appengineXml, XPathConstants.STRING);
+        props.setProperty(EmailyConfig.APPENGINE_APP_NAME, (String) appName);
+        Object appVersion = xPath.compile("//*[local_nam,e()='version']")
+            .evaluate(appengineXml, XPathConstants.STRING);
+        props.setProperty(EmailyConfig.APPENGINE_APP_VERSION, (String) appVersion);
+      } catch (SAXException e) {
+        throw new RuntimeException("Cannot parse appengine-web.xml", e);
+      } catch (IOException e) {
+        throw new RuntimeException("Error reading appengine-web.xml", e);
+      } catch (ParserConfigurationException e) {
+        throw new RuntimeException("Error during XML parsing", e);
+      } catch (XPathExpressionException e) {
+        throw new RuntimeException("Error during XML parsing", e);
+      }
+    }
+    return props;
+  }
 }
